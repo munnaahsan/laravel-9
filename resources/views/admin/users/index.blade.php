d96639eb5d49edd7846ae002fcd1d16768ffaf87
Munna Ahsan <54517912+munnaahsan@users.noreply.github.com>
2022-03-23 13:13:14 +0600

    'users'

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,60 @@
+<x-admin-layout>
+    <div class="py-12 w-full">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="flex flex-col p-2 bg-slate-100">
+                    <div class="-my-2 overflow-x-auto sm:-mx-6 lg-mx-8">
+                        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                <table class="min-w-full divide-y divide-gray-200">
+                                    <thead class="bg-gray-50">
+                                    <tr>
+                                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" scope="col">Name</th>
+                                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" scope="col">Email</th>
+                                        <th scope="col" class="relative px-6 py-3">
+                                            <span class="sr-only">Edit</span>
+                                        </th>
+                                    </tr>
+                                    </thead>
+                                    <tbody class="bg-white divide-y divide-gray-200">
+                                    @foreach($users as $user)
+                                        <tr>
+                                            <td class="px-6 py-4 whitespace-nowrap">
+                                                <div class="flex items-center">
+                                                    {{ $user->name }}
+                                                </div>
+                                            </td>
+                                            <td class="px-6 py-4 whitespace-nowrap">
+                                                <div class="flex items-center">
+                                                    {{ $user->email }}
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="flex justify-end">
+                                                    <div class="flex space-x-2 bg-slate-100">
+                                                        <a class="px-4 py-2 bg-blue-500 hover:bg-blue-700 text-white rounded-md" href="">Roles</a>
+                                                        <a class="px-4 py-2 bg-blue-500 hover:bg-blue-700 text-white rounded-md" href="">Permission</a>
+                                                        <form
+                                                            method="POST"
+                                                            action="{{ route('admin.users.destroy', $user->id) }} "
+                                                            class="px-4 py-2 bg-red-500 hover:bg-red-700 text-white rounded-md"
+                                                            onsubmit="return confirm('Are You Sure...?');" >
+                                                            @csrf
+                                                            @method('DELETE')
+                                                            <button type="submit">Delete</button>
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-admin-layout>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\IndexController;
 use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\PermissionController;
+use App\Http\Controllers\Admin\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,6 +34,8 @@ Route::middleware(['auth', 'role:admin'])->name('admin.')->prefix('admin')->grou
     Route::resource('/permissions', PermissionController::class);
     Route::post('/permissions/{permission}/roles', [PermissionController::class, 'assignRole'])->name('permissions.roles');
     Route::delete('/permissions/{permission}/roles/{role}', [PermissionController::class, 'removeRole'])->name('permissions.roles.remove');
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
 });
 
 require __DIR__.'/auth.php';
